52b3f7f8fc7e9c5b5d949de3b613d44c2963f443
Carlos Apolaya <temis_it@hotmail.com>
2023-03-29 17:32:41 -0500

    nue

--- src/utils/data.js
+++ src/utils/data.js
@@ -25,5 +25,42 @@ export const data =[
        enlaces:['https://docs.google.com/presentation/d/1_UZoRpz-GdEukijojjUXkVv9v3pP1puw/edit?usp=share_link&ouid=114172849724389982127&rtpof=true&sd=true']
 
 
+    },
+    {
+        id:3,
+       thumbnail:"https://www.thelanguagegallery.com/media/1261/blog-asking-directions-in-a-foreign-country_blogheader.jpg",
+       title:"Lost and Found: Essential English Phrases for Asking and Giving Directions 🗺️",
+       subtitle:"A Comprehensive Guide to Navigating Your Way Around English-Speaking Places 🚶‍♀️",
+
+       dateAdded:"wswqe",
+       channel:"sdasd",
+       description:"we'll show you how to confidently navigate English-speaking places using essential English phrases for asking and giving directions. From finding your way to the nearest restaurant to getting directions to the nearest tourist attraction, we'll equip you with the language tools you need to explore new places with ease.",
+       enlaces:['https://docs.google.com/presentation/d/1Xkzzm-SaDCTx75migyE8PLa_qbxRdcgl/edit?usp=sharing&ouid=114172849724389982127&rtpof=true&sd=true']
+
+
+    },
+    {
+        id:4,
+       thumbnail:"https://www.businesstelegraph.co.uk/wp-content/uploads/2021/05/Best-Ways-to-Choose-A-Medical-School-for-Your-Medical-Degree.jpeg",
+       title:"🏥 Surviving Your Hospital Visit: Essential Vocabulary and Useful Phrases to Know 💬",
+       subtitle:"📚 A Comprehensive Guide to Navigating Your Next Hospital Visit with Confidence 🏥",
+
+       dateAdded:"wswqe",
+       channel:"sdasd",
+       description:" we will cover everything you need to know to successfully communicate during your hospital visit. From basic medical terminology to practical phrases for communicating with medical professionals, we'll equip you with the language tools you need to feel empowered and informed. Whether you're a patient or a visitor, this guide will help you navigate your hospital visit with ease and confidence. 💪",
+       enlaces:['https://docs.google.com/presentation/d/1jaFQMkG867NsorKPe_64SvgtZ73GGRR1/edit?usp=sharing&ouid=114172849724389982127&rtpof=true&sd=true']
+
+    },
+    {
+        id:5,
+        thumbnail:"https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2022/10/Paralegal_vs._Lawyer.jpeg.jpg",
+       title:"👨‍⚖️ Navigating Legal Proceedings: Essential Vocabulary and Useful Phrases for Courtroom Success ⚖️",
+       subtitle:"📚 A Comprehensive Guide to Understanding Legal Jargon and Communicating Effectively in Court 🗣️",
+
+       dateAdded:"wswqe",
+       channel:"sdasd",
+       description:"we will cover the essential vocabulary and useful phrases needed to navigate legal proceedings in an English-speaking courtroom. From understanding common legal terms to effectively communicating with judges, lawyers, and other court officials, we'll equip you with the language tools you need to succeed in your legal case. Whether you're representing yourself or seeking legal representation, this guide will help you communicate with confidence in the courtroom. So, get ready to learn the essential legal vocabulary and phrases that will help you navigate your way through the courtroom with ease and success. 🤓",
+       enlaces:['https://docs.google.com/presentation/d/1IUm3ITKKIZL4AEJ1cF4Zpaujd5tcmk85/edit?usp=share_link&ouid=114172849724389982127&rtpof=true&sd=true']
+
     }
 ]
