71f2835f3381600055c95512a1e7716e96e84f5f
Carlos Apolaya <temis_it@hotmail.com>
2023-04-10 20:07:37 -0500

    fa

--- src/pages/ErroresB2.jsx
+++ src/pages/ErroresB2.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../css/posts.css"
+import Descargables from './Descargables';
 const ErroresB2 = () => {
   return (
     <div className='container-fluid'>
@@ -156,27 +157,7 @@ const ErroresB2 = () => {
           
 
          </div>
-        <div className='col-lg-2 '>
-          <h2>Descargables B2-C1</h2>
-          <p>Aquí te dejo tarjetas descargables para que tu estudio sea muy productivo. Algunas están completamente en inglés y otras los tienes en versión inglés- español para que aprendas como prefieras.</p>
-          <ul>
-            <li>
-              <a href="https://drive.google.com/file/d/1pqGlxrpQYa2XV_w5CzARY1y-mlXPrz_A/view?usp=share_link">VOCABULARY: verbos y adjetivos B2-C1 English-Spanish</a>
-            </li>
-            <li>
-              <a href="https://drive.google.com/file/d/1MhuXPI8Hmx1JPJhForchbZBK3WrxrD7y/view?usp=share_link">VOCABULARY: verbs and adjectives B2-C1 English</a>
-            </li>
-            <li>
-              <a href="https://drive.google.com/file/d/1-KpxWoCwc49FUS-jMrExyO-zGuZwlSBR/view?usp=share_link">PHRASAL VERBS- GET, PUT, TAKE, GO English- Spanish</a>
-            </li>
-            <li>
-              <a href="https://drive.google.com/file/d/1fYWkfPNsW3W7xSNpSW-Dq2jJ7h7HZREH/view?usp=share_link">PHRASAL VERBS- GET, PUT, TAKE, GO English</a>
-            </li>
-            <li>
-              <a href="https://drive.google.com/file/d/1G5ybFyesyYXglzb8nnUlvX0Zi3iI1vGd/view?usp=share_link">EXPRESSIONS WITH NO – C1</a>
-            </li>
-          </ul>
-        </div>
+        <Descargables></Descargables>
       </div>
     </div>
   )
--- src/pages/PhrasesAtWork.jsx
+++ src/pages/PhrasesAtWork.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../css/posts.css"
+import Descargables from './Descargables';
 const PhrasesAtWork = () => {
   return (
     <div className='container-fluid'>
@@ -248,27 +249,7 @@ Thank you so much for watching and we hope to see you in the next lesson! Take c
           
 
          </div>
-         <div className='col-lg-2 '>
-          <h2>Descargables B2-C1</h2>
-          <p>Aquí te dejo tarjetas descargables para que tu estudio sea muy productivo. Algunas están completamente en inglés y otras los tienes en versión inglés- español para que aprendas como prefieras.</p>
-          <ul>
-            <li>
-              <a href="https://drive.google.com/file/d/1pqGlxrpQYa2XV_w5CzARY1y-mlXPrz_A/view?usp=share_link">VOCABULARY: verbos y adjetivos B2-C1 English-Spanish</a>
-            </li>
-            <li>
-              <a href="https://drive.google.com/file/d/1MhuXPI8Hmx1JPJhForchbZBK3WrxrD7y/view?usp=share_link">VOCABULARY: verbs and adjectives B2-C1 English</a>
-            </li>
-            <li>
-              <a href="https://drive.google.com/file/d/1-KpxWoCwc49FUS-jMrExyO-zGuZwlSBR/view?usp=share_link">PHRASAL VERBS- GET, PUT, TAKE, GO English- Spanish</a>
-            </li>
-            <li>
-              <a href="https://drive.google.com/file/d/1fYWkfPNsW3W7xSNpSW-Dq2jJ7h7HZREH/view?usp=share_link">PHRASAL VERBS- GET, PUT, TAKE, GO English</a>
-            </li>
-            <li>
-              <a href="https://drive.google.com/file/d/1G5ybFyesyYXglzb8nnUlvX0Zi3iI1vGd/view?usp=share_link">EXPRESSIONS WITH NO – C1</a>
-            </li>
-          </ul>
-        </div>
+        <Descargables></Descargables>
       </div>
     </div>
   )
