4ffd37a4f0acbc189f29de9bbaf61682d5b2198b
Carlos Apolaya <temis_it@hotmail.com>
2023-04-17 19:57:36 -0500

    asd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ import cliente6 from '../img/clients/client-6.png';
 const Home = () => {
   return (
     <>
-      <div class="hero vh-100 d-flex align-items-center">
+      <div class="hero-main vh-100 d-flex align-items-center">
     <div class="container">
         <div class="row">
             <div class="col-lg-7 mx-auto text-center">
