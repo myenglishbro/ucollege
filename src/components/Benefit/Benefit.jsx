906bd04e17674ca0c7e1dc3a18d580f215f61cba
Carlos Apolaya <temis_it@hotmail.com>
2023-06-30 17:01:11 -0500

    sd

--- src/components/Benefit/Benefit.jsx
+++ src/components/Benefit/Benefit.jsx
@@ -8,11 +8,13 @@ const Benefit = () => {
         <input type="radio" name="abrir" id="acc1"/>
         <label for="acc1">
             <h2>01</h2>
-            <h3>¿Cómo me inscribo?</h3>
+            <h3>¿Qué nivel de inglés necesito para apuntarme?</h3>
         </label>
         <div class="content">
             <p>
-                Desde nuestra pagina web codigo369.com, el curso se llama "html y css intensivo camino hacia react"
+            El reto You Speak está recomendado para estudiantes a partir de un nivel de inglés Intermediate (B1- B2).
+También te lo recomendamos si te estás preparando para obtener el nivel Advanced (C1) y sientes que necesitas expandir tu vocabulario y entrenar tu expresión oral.
+¿No estás seguro de cuál es tu nivel? Puedes descubrirlo con este test: Test your English.
               </p>
         </div>
     </div>
@@ -20,11 +22,25 @@ const Benefit = () => {
         <input type="radio" name="abrir" id="acc2"/>
         <label for="acc2">
             <h2>02</h2>
-            <h3>¿Cómo me inscribo?</h3>
+            <h3>¿Cómo son las clases del curso?</h3>
         </label>
         <div class="content">
             <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis ut dolorem facere ullam, maxime vel molestias accusamus dolores delectus, soluta perspiciatis praesentium blanditiis officiis. Exercitationem in doloribus esse perferendis similique?
+            Te hemos preparado actividades para cada uno de los 30 días del curso. Yee-haw!
+
+Cada día tendrás acceso a nuevas clases y entrenamientos para poner tu expresión oral en forma.
+
+Todo este contenido se ha preparado con antelación para que puedas acceder a la hora del día que más te convenga.
+
+La idea es ir haciendo juntos la clase correspondiente a cada día y que puedas compartir en la escuela tus tareas con los demás estudiantes y profes.
+
+Este es un ejemplo de un día:
+
+💂🏻‍♀️ Immerse yourself: sumérgete en el idioma y expande tu vocabulario con nuestros divertidos vídeos de preguntas y respuestas.
+
+💪 Train your fluency: entrena tu fluidez y tu pronunciación con nuestros workouts diarios y empieza a sonar mucho más natural y como un nativo.
+
+👄 Unlock your voice: llegó la hora de la verdad. Tu reto de speaking diario. You Speak! 🫵 Graba tu tarea en vídeo y compártela en la comunidad con los demás estudiantes. Pregúntanos cualquier duda que tengas sobre tu tarea o las lecciones.
               </p>
         </div>
     </div>
@@ -32,11 +48,12 @@ const Benefit = () => {
         <input type="radio" name="abrir" id="acc3"/>
         <label for="acc3">
             <h2>03</h2>
-            <h3>¿Cómo me inscribo?</h3>
+            <h3>¿Cuánto tiempo necesito dedicar cada día?</h3>
         </label>
         <div class="content">
             <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis ut dolorem facere ullam, maxime vel molestias accusamus dolores delectus, soluta perspiciatis praesentium blanditiis officiis. Exercitationem in doloribus esse perferendis similique?
+            30 minutos al día son suficientes para ver el vídeo, escuchar el audio y hacer tu reto de speaking, aunque este tiempo puede variar según el nivel de cada estudiante. 
+Una vez acabes el curso, te recomendamos volver a repasar las lecciones, hacer los entrenamientos y los ejercicios prácticos para ver los resultados y ampliar tus conocimientos de forma natural, mediante la inmersión y contacto diario con el idioma. 👌
               </p>
         </div>
     </div>
@@ -44,11 +61,19 @@ const Benefit = () => {
         <input type="radio" name="abrir" id="acc4"/>
         <label for="acc4">
             <h2>04</h2>
-            <h3>¿Cómo me inscribo?</h3>
+            <h3>¿Se pueden descargar los materiales para aprender offline?</h3>
         </label>
         <div class="content">
             <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis ut dolorem facere ullam, maxime vel molestias accusamus dolores delectus, soluta perspiciatis praesentium blanditiis officiis. Exercitationem in doloribus esse perferendis similique?
+            Yes, you can! ✌️
+
+Si deseas trabajar sin estar conectado a internet, simplemente envíanos tu petición a: temis_it@hotmail.com y te enviaremos los enlaces de descarga de los materiales del curso. 
+
+Podrás solicitar las descarga de materiales una vez haya transcurrido el período de prueba de 15 días.
+
+Bear in mind
+
+Ten en cuenta que podemos enviarte todos los materiales (audios, worksheets y ejercicios) excepto los vídeos, ya que se trata de archivos muy pesados.
               </p>
         </div>
     </div>
