5906dbee25189c87e34a4c8e259ccc770535d7a8
Carlos Apolaya <temis_it@hotmail.com>
2023-10-04 13:46:12 -0500

    nuevo

--- src/pages/Horario.jsx
+++ src/pages/Horario.jsx
@@ -77,8 +77,8 @@ const Horario = () => {
       <li class="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer">04:00-05:00 pm 🟢 </li>
       <li class="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer">05:00-06:00 pm 🟢 </li>
       <li class="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer">06:00-07:00 pm 🟢 </li>
-      <li class="font-poppins font-normal text-[16px] leading-[24px] text-dimBusy hover:text-secondary cursor-pointer">7:00-08:00 pm 🟢 </li>
-      <li class="font-poppins font-normal text-[16px] leading-[24px] text-dimBusy hover:text-secondary cursor-pointer">8:00-09:00 pm 🟢 </li>
+      <li class="font-poppins font-normal text-[16px] leading-[24px] text-dimBusy hover:text-terciary cursor-pointer">7:00-09:00 pm 🔴 </li>
+  
       <li class="font-poppins font-normal text-[16px] leading-[24px] text-dimBusy hover:text-terciary cursor-pointer">9:15-10:45 pm 🔴 </li>
       <li class="font-poppins font-normal text-[16px] leading-[24px] text-dimBusy hover:text-terciary cursor-pointer">11:00-12:00 pm 🔴 </li>
       <li class="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer">12:00-01:00 am 🟢 </li>
@@ -145,7 +145,7 @@ const Horario = () => {
       <div class="col-lg-2">
       <h5 class="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ">Sunday 💪</h5>
       <ul class="list-group list-group-flush">
-      <li class="font-poppins font-normal text-[16px] leading-[24px] text-dimBusy hover:text-secondary cursor-pointer">09:00-11:00 am 🟢 </li>
+      <li class="font-poppins font-normal text-[16px] leading-[24px] text-dimBusy hover:text-terciary cursor-pointer">09:00-11:00 am 🔴 </li>
       <li class="font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer">12:00-01:00 pm 🟢 </li>
 
 
