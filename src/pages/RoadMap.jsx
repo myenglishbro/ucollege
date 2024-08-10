584ec681623b422819bce1d85fc6fb1cf75424b2
Carlos Apolaya <temis_it@hotmail.com>
2024-08-10 11:33:16 -0500

    AS

--- src/pages/RoadMap.jsx
+++ src/pages/RoadMap.jsx
@@ -23,7 +23,7 @@ const RoadMap = () => {
   };
 
   const handleMostrarComponente = () => {
-    const validPasswords = ['udemystudent', 'andresjaramillo', 'davidbenites', 'jrvchoche', 'cesarhurtado', 'roycondori', 'maxcontreras','rosamamani','manuellopez','teachercarlos']; 
+    const validPasswords = ['udemystudent', 'andresjaramillo', 'davidbenites', 'jrvchoche', 'cesarhurtado', 'roycondori', 'maxcontreras','rosamamani','manuellopez','teachercarlos','diegosegovia']; 
     if (validPasswords.includes(codigo)) {
       setMostrarComponente(true);
     } else {
