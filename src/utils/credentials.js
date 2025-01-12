aae4b5368088a783d653443e45fc9d64ca074a7d
Carlos Apolaya <temis_it@hotmail.com>
2025-01-12 18:45:37 -0500

    WQ

--- src/utils/credentials.js
+++ src/utils/credentials.js
@@ -74,5 +74,6 @@ export const validCredentials = [
        { expirationDate:"09/02/25",usuario: 'giancarlo.95.p@gmail.com', password: 'tok2025', realname: 'Giancarlo Peña ', img: 'https://i.ibb.co/chn6rvR/Dise-o-sin-t-tulo-3.png' }, // No image
        { expirationDate:"12/01/25",usuario: 'usergratis', password: 'meb25', realname: 'Modo de Prueba ', img: 'https://i.ibb.co/chn6rvR/Dise-o-sin-t-tulo-3.png' }, // No image
        {  expirationDate:"03/04/25",nivel:"6",usuario: 'zaidaz', password: 'zorrilla24', realname: 'Zaida Zorrilla', img: 'https://i.ibb.co/chn6rvR/Dise-o-sin-t-tulo-3.png' }, // No image
+       {  expirationDate:"12/02/25",nivel:"solicitar examen",usuario: 'alexmonte', password: 'tiktok25', realname: 'Alexander Montes', img: 'https://i.ibb.co/chn6rvR/Dise-o-sin-t-tulo-3.png' }, // No image
 
     ];
