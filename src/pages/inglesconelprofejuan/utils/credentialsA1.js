3a06e986585a063bfe64beb8a77064ca4c41bc1b
Carlos Apolaya <temis_it@hotmail.com>
2025-02-03 16:10:50 -0500

    AS

--- src/pages/inglesconelprofejuan/utils/credentialsA1.js
+++ src/pages/inglesconelprofejuan/utils/credentialsA1.js
@@ -2,6 +2,7 @@ export const validCredentialsa1 = [
    
     { expirationDate:"Esta cuenta es Gratuita",nivel:"C2",usuario: 'juan', password: '2025', realname: 'Cuenta Gratuita', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
     // { expirationDate:"28/02/26",nivel:"solicitar examen",usuario: 'erix', password: 'erixpereyra', realname: 'Erix Pereyra S.', img:'https://i.ibb.co/4n9497yv/393875820-313689618102677-771283.png'},
+    { expirationDate:"2026",nivel:"solicitar examen",usuario: 'oly', password: 'montiel', realname: 'Oly Montiel', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
 
     
     ];
