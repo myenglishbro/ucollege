1ca1ef2734a0be7132202cabd5818b32160f2aa2
Carlos Alberto Apolaya Sanchez <109998878+myenglishbro@users.noreply.github.com>
2025-02-05 14:06:01 -0500

    Me Update credentialsA1.js

--- src/pages/inglesconelprofejuan/utils/credentialsA1.js
+++ src/pages/inglesconelprofejuan/utils/credentialsA1.js
@@ -5,5 +5,5 @@ export const validCredentialsa1 = [
     { expirationDate:"2026",nivel:"solicitar examen",usuario: 'oly', password: 'montiel', realname: 'Oly Montiel', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
     { expirationDate:"2026",nivel:"solicitar examen",usuario: 'maria', password: 'cuevas', realname: 'María Andreina Cuevas', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
 
-    
+    { expirationDate:"2026",nivel:"solicitar examen",usuario: 'pablo', password: 'martinez', realname: 'Pablo Martinez', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
     ];
