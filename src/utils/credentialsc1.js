36d1b4b297ded8ed31e718a2d6b6dea0c97f5fff
Carlos Apolaya <temis_it@hotmail.com>
2025-02-04 20:17:40 -0500

    AS

--- src/utils/credentialsc1.js
+++ src/utils/credentialsc1.js
@@ -2,7 +2,7 @@ export const validCredentialsc1 = [
    
     { expirationDate:"05/12/35",nivel:"C2",usuario: 'xio', password: 'meb1', realname: 'MyEnglishBro!', img:'https://i.ibb.co/c3ZvTqW/My-english-bro-Logo-09.png'},
     // { expirationDate:"28/02/26",nivel:"solicitar examen",usuario: 'erix', password: 'erixpereyra', realname: 'Erix Pereyra S.', img:'https://i.ibb.co/4n9497yv/393875820-313689618102677-771283.png'},
-    {  expirationDate:"25/02/25",nivel:"solicitar examen",usuario: 'kath', password: 'b22025', realname: 'Katherine Contreras', img: 'https://media-lim1-1.cdn.whatsapp.net/v/t61.24694-24/465207514_446901671757863_2117388870071625587_n.jpg?ccb=11-4&oh=01_Q5AaIF7teZ1U8nPkfyBbVS2YcvB4odLF6moosqu05oPIcNuZ&oe=67AFC92A&_nc_sid=5e03e0&_nc_cat=100' }, // No image
+    // {  expirationDate:"25/02/25",nivel:"solicitar examen",usuario: 'kath', password: 'b22025', realname: 'Katherine Contreras', img: 'https://media-lim1-1.cdn.whatsapp.net/v/t61.24694-24/465207514_446901671757863_2117388870071625587_n.jpg?ccb=11-4&oh=01_Q5AaIF7teZ1U8nPkfyBbVS2YcvB4odLF6moosqu05oPIcNuZ&oe=67AFC92A&_nc_sid=5e03e0&_nc_cat=100' }, // No image
 
     
     ];
