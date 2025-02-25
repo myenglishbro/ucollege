29e3c2e8f8a1f361db47fdea080382452671397a
Carlos Alberto Apolaya Sanchez <109998878+myenglishbro@users.noreply.github.com>
2025-02-24 20:30:28 -0500

    Update eww credentialsA1.js

--- src/pages/inglesconelprofejuan/utils/credentialsA1.js
+++ src/pages/inglesconelprofejuan/utils/credentialsA1.js
@@ -1,7 +1,7 @@
 export const validCredentialsa1 = [
    
     { expirationDate:"Esta cuenta es Gratuita",nivel:"C2",usuario: 'juan', password: '2025', realname: 'Cuenta Gratuita', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
-    // { expirationDate:"28/02/26",nivel:"solicitar examen",usuario: 'erix', password: 'erixpereyra', realname: 'Erix Pereyra S.', img:'https://i.ibb.co/4n9497yv/393875820-313689618102677-771283.png'},
+     { expirationDate:"2026",nivel:"solicitar examen",usuario: 'milena', password: 'silva', realname: 'Milena  Silva', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
     { expirationDate:"2026",nivel:"solicitar examen",usuario: 'oly', password: 'montiel', realname: 'Oly Montiel', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
     { expirationDate:"2026",nivel:"solicitar examen",usuario: 'maria', password: 'cuevas', realname: 'María Andreina Cuevas', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
 
