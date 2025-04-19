717e2550682e2bf66291b8f765891ef281f794ec
Carlos Apolaya <temis_it@hotmail.com>
2025-04-19 18:03:27 -0500

    AS

--- src/utils/credentials.js
+++ src/utils/credentials.js
@@ -74,5 +74,6 @@ export const validCredentials = [
        { expirationDate:"19/05/25",nivel:"B2",usuario: 'juan', password: 'dgarcia', realname: 'Juan angel Díaz García',img:'https://i.ibb.co/chn6rvR/Dise-o-sin-t-tulo-3.png'},
        { expirationDate:"19/05/25",nivel:"B2",usuario: 'nicolas', password: 'agarcia', realname: 'Nicolás Antía García',img:'https://i.ibb.co/chn6rvR/Dise-o-sin-t-tulo-3.png'},
        { expirationDate:"19/05/25",nivel:"A1",usuario: 'luz', password: 'garcia', realname: 'Luz Stella García',img:'https://i.ibb.co/chn6rvR/Dise-o-sin-t-tulo-3.png'},
+       { expirationDate:"19/05/25",nivel:"A1",usuario: 'roxana', password: 'gonzales', realname: 'Roxana Gonzales',img:'https://i.ibb.co/chn6rvR/Dise-o-sin-t-tulo-3.png'},
 
     ];
