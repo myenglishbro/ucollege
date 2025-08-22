fdc51731ab566ccaec6e16927bd5b67dd3e1ef6c
Carlos Apolaya <temis_it@hotmail.com>
2025-08-22 11:13:38 -0500

    AS

--- src/utils/credentials.js
+++ src/utils/credentials.js
@@ -81,5 +81,6 @@ export const validCredentials = [
 //ACCESO DE POR VIDA
              { expirationDate:"Unlimited Account",nivel:"B2",usuario: 'adrian', password: 'aliaga', realname: 'Adrian Aliaga',img:'https://i.ibb.co/DHQLnHbK/ADRIAN.png'},
              { expirationDate:"Unlimited Account",nivel:"B2",usuario: 'gabriela', password: 'torres', realname: 'Gabriela Torres',img:'https://i.ibb.co/c3ZvTqW/My-english-bro-Logo-09.png'},
+             { expirationDate:"Unlimited Account",nivel:"B2",usuario: 'herberth', password: 'jara', realname: 'Herberth Jara Huaman',img:'https://i.ibb.co/c3ZvTqW/My-english-bro-Logo-09.png'},
 
     ];
