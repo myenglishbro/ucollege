623039322f4f85308ff0a4568b890cba92d720e9
Carlos Apolaya <temis_it@hotmail.com>
2025-10-01 08:06:09 -0500

    AS

--- src/utils/credentialsA1Juan.js
+++ src/utils/credentialsA1Juan.js
@@ -39,6 +39,7 @@ export const validCredentialsa1 = [
 
    { expirationDate:"2026",nivel:"solicitar examen",usuario: 'patricia', password: 'vizcarra', realname: 'Patricia Vizcarra', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
    { expirationDate:"2026",nivel:"solicitar examen",usuario: 'leonarda', password: 'sanchez', realname: 'Leonarda Sanchez', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
+   { expirationDate:"2026",nivel:"solicitar examen",usuario: 'maria', password: 'loja', realname: 'Maria Loja', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
 
 
 ];
