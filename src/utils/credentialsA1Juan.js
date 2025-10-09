87653ca47a927985ce4d126ff32e85a892dff2c3
Carlos Alberto Apolaya Sanchez <109998878+myenglishbro@users.noreply.github.com>
2025-10-08 19:14:27 -0500

    Actualizar credentialsA1Juan.js

--- src/utils/credentialsA1Juan.js
+++ src/utils/credentialsA1Juan.js
@@ -41,6 +41,7 @@ export const validCredentialsa1 = [
    { expirationDate:"2026",nivel:"solicitar examen",usuario: 'leonarda', password: 'sanchez', realname: 'Leonarda Sanchez', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
    { expirationDate:"2026",nivel:"solicitar examen",usuario: 'maria', password: 'loja', realname: 'Maria Loja', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
    { expirationDate:"2026",nivel:"solicitar examen",usuario: 'nickol', password: 'lopez', realname: 'Nickol Lopez', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
+      { expirationDate:"2026",nivel:"solicitar examen",usuario: 'osneider', password: 'rojas', realname: 'Osneider Rojas', img:'https://i.ibb.co/9HLwLnzb/logo.jpg'},
 
 
 ];
