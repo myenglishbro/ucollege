b66ed3c4c6fff8b75dd2566c81b63bfb6e010cf2
Carlos Apolaya <temis_it@hotmail.com>
2026-01-07 08:00:39 -0500

    AS

--- src/utils/credentials.js
+++ src/utils/credentials.js
@@ -2,7 +2,7 @@ export const validCredentials = [
    
     
 
-      { expirationDate:"Acceso Ilimitado",nivel:"premium",usuario: 'lia', password: 'adriana', realname: 'Prueba Gratuita', img: 'https://i.ibb.co/chn6rvR/Dise-o-sin-t-tulo-3.png' }, // No image
+      { expirationDate:"Acceso Ilimitado",nivel:"premium",usuario: 'carlos', password: 'app', realname: 'Prueba Gratuita', img: 'https://i.ibb.co/chn6rvR/Dise-o-sin-t-tulo-3.png' }, // No image
 
     
   
